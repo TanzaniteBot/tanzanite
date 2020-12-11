b8203c71f36493db0dfed95e2623494366f5961e
Tyman <=>
2020-12-11 16:37:25 -0500

    recompile on reload

--- src/commands/owner/reload.ts
+++ src/commands/owner/reload.ts
@@ -1,6 +1,6 @@
 import { Command, AkairoError } from 'discord-akairo';
 import { Message } from 'discord.js';
-import { createTextChangeRange } from 'typescript';
+import { exec } from "child_process";
 
 export default class ReloadCommand extends Command {
 	public constructor() {
@@ -41,44 +41,49 @@ export default class ReloadCommand extends Command {
 					time: 30000
 				}
 			}
-			return {type, id}
+			return { type, id }
 		}
-		else return {type}
+		else return { type }
 	}
 
 	public exec(message: Message, { type, id }: { type: string, id: string }) {
-		switch (type) {
-			case 'category':
-				try {
-					this.handler.findCategory(id).reloadAll();
-				} catch (e) {
-					return message.util.send(e.message);
-				}
-				break;
-			case 'all':
-				try {
-					this.handler.reloadAll()
-				} catch (e) {
-					return message.util.send(e.message);
-				}
-				break;
-			case 'command':
-				try {
-					this.handler.reload(id)
-				} catch (e) {
-					return message.util.send(e.message);
-				}
-				break;
-			case 'inhibitor':
-				try {
-					this.handler.inhibitorHandler.reload(id)
-				} catch (e) {
-					return message.util.send(e.message);
-				}
-				break;
-			default:
-				return message.util.send("Wtf how did this happen")
-		}
-		message.util.send(`Reloaded ${(id == undefined && type == "all") ? "all" : id} 🔁`)
+		exec('npx tsc', (error, stdout, stderr) => {
+			if (error) {
+				return message.util.send(`Error recompiling, \`${error.name}\``)
+			}
+			switch (type) {
+				case 'category':
+					try {
+						this.handler.findCategory(id).reloadAll();
+					} catch (e) {
+						return message.util.send(e.message);
+					}
+					break;
+				case 'all':
+					try {
+						this.handler.reloadAll()
+					} catch (e) {
+						return message.util.send(e.message);
+					}
+					break;
+				case 'command':
+					try {
+						this.handler.reload(id)
+					} catch (e) {
+						return message.util.send(e.message);
+					}
+					break;
+				case 'inhibitor':
+					try {
+						this.handler.inhibitorHandler.reload(id)
+					} catch (e) {
+						return message.util.send(e.message);
+					}
+					break;
+				default:
+					return message.util.send("Wtf how did this happen")
+			}
+			message.util.send(`Reloaded ${(id == undefined && type == "all") ? "all" : id} 🔁`)
+		})
 	};
 };
