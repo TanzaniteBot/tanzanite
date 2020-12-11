3aba41d4acc775539ae6941f7fb492f7b1800376
Tyman <=>
2020-12-11 15:20:06 -0500

    reload command

--- src/commands/owner/reload.ts
+++ src/commands/owner/reload.ts
@@ -1,4 +1,4 @@
-import { Command } from 'discord-akairo';
+import { Command, Category } from 'discord-akairo';
 import { Message } from 'discord.js';
 
 export default class ReloadCommand extends Command {
@@ -9,17 +9,18 @@ export default class ReloadCommand extends Command {
             args: [
                 {
                     id: 'type',
-                    type: 'string',
+                    type: /(?:command|category|inhibitor|listener|all)/i,
                     default: 'command',
                 },
                 {
                     id: 'id',
-                    type: 'string',
+					type: 'string',
+					default: null
                 }
             ],
             description: {
                 content: 'Use the command to reload stuff in the bot',
-                usage: 'reload < category | command | inhibitor | listener > <id of whatever thing you want to reload>',
+                usage: 'reload < category | command | inhibitor | listener | all > <id of whatever thing you want to reload>',
                 examples: [
                     'reload module owner',
                     'reload ping'
@@ -30,8 +31,24 @@ export default class ReloadCommand extends Command {
             ownerOnly: true,
         });
     };
-    public exec(message: Message, { type, id }: {type:String, id:String}) {
-        Command.reload(id, sads)
+    public exec(message: Message, { type, id }: {type:string, id:string}) {
+		if (id == null && type != 'all') return message.util.send("Must give an id!");
+        switch (type) {
+			case 'category':
+				this.handler.findCategory(id).reloadAll();
+				break;
+			case 'all':
+				this.handler.reloadAll()
+				break;
+			case 'command':
+				this.handler.reload(id)
+				break;
+			case 'inhibitor':
+				this.handler.inhibitorHandler.reload(id)
+				break;
+			default:
+				return message.util.send("Wtf how did this happen")
+		}
         message.util.send(`Reloaded ${id} 🔁`)
     };
 };
