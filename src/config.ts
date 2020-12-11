41af50b2359fe30b26351b8ced2d5ac3b68becd5
Tyman <=>
2020-12-11 16:39:43 -0500

    gitignore config

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export let token: string = 'NzY3NDc4MzU5MzQ4NzQwMTQ4.X4yf6w.ArVSH2GWUfY8UgW0Ylx0aWCet4k'
-export let prefix: string = '-'
-export let owners: string[] | string = [
-    '464970779944157204', /* TrashCan */
-    '322862723090219008', /* IRONM00N */
-	'211288288055525376', /* Moul */
-	'487443883127472129', /* Tyman, for testing, you can remove this if I forget to */
-]
