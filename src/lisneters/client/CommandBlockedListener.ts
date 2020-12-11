f49ac8d423ecb8f22a751f84a9e6e486be26c1ae
Tyman <=>
2020-12-11 16:57:17 -0500

    commmand blocked text

---
+++ src/lisneters/client/CommandBlockedListener.ts
@@ -0,0 +1,22 @@
+import { Listener, Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+export default class ReadyListener extends Listener {
+    public constructor() {
+        super('commandBlocked', {
+            emitter: 'commandHandler',
+            event: 'commandBlocked',
+            category: 'client'
+        });
+    }
+
+    public exec(message: Message, command: Command, reason: string): void {
+		switch (reason) {
+			case 'owner':
+				message.util.send(`You must be an owner to run command \`${message.util.parsed.command}\``)
+				break
+			default:
+				message.util.send(`Command blocked with reason \`${reason}\``)
+		}
+	}
+};
