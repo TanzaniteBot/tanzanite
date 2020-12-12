70ca26a6dd185bec444ab3210d782ffacfc65f5c
Tyman <=>
2020-12-11 19:22:52 -0500

    ping command

---
+++ src/commands/info/ping.ts
@@ -0,0 +1,26 @@
+import { Command } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js';
+
+export default class ReloadCommand extends Command {
+    public constructor() {
+        super('ping', {
+            aliases: ['ping'],
+            category: 'info',
+            ratelimit: 4,
+            cooldown: 4000,
+        });
+    };
+    public async exec(message: Message) {
+		const replyMsg: Message = await message.util.send("Pong!")
+		const timestamp: number = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp;
+		const latency = `\`\`\`\n ${Math.floor(replyMsg.createdTimestamp - timestamp)}ms \`\`\``;
+		const apiLatency = `\`\`\`\n ${Math.round(message.client.ws.ping)}ms \`\`\``;
+		const embed: MessageEmbed = new MessageEmbed()
+			.setTitle('Pong!  🏓')
+			.addField('Latency', latency, true)
+			.addField('API Latency', apiLatency, true)
+			.setFooter(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
+			.setTimestamp();
+		replyMsg.edit(embed)
+    };
+};
