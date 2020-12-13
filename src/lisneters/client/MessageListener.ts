c1e73e673a99eee5b4788addd774727d83f612cf
TrashCan <samuel2007davis@gmail.com>
2020-12-13 03:12:38 -0500

    dm shit

---
+++ src/lisneters/client/MessageListener.ts
@@ -0,0 +1,27 @@
+import { Listener, AkairoClient } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js'
+
+export default class ReadyListener extends Listener {
+    public constructor() {
+        super('MessageListener', {
+            emitter: 'client',
+            event: 'message',
+            category: 'client'
+        });
+    }
+
+    public exec(message: Message) {
+        // on dm
+        if(message.channel.type === 'dm') {
+            if (message.author.bot) return
+            // add a thing to send it so a specific channel
+            const dmlogembed = new MessageEmbed()
+            .setAuthor(`From: ${message.author.username}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
+            .setDescription(`**DM:**\n${message}`)
+            .setTimestamp()
+            .setFooter(`ID • ${message.author.id}`)
+            message.channel.send(dmlogembed);
+        }
+        // put other shit here
+    };
+};
