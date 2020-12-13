8b5c4103e45440735cc695df3fcc0f3cc1f79594
Tyman <=>
2020-12-13 01:45:50 -0500

    might work idk I have to sleep

--- src/lisneters/commands/CommandErrorListener.ts
+++ src/lisneters/commands/CommandErrorListener.ts
@@ -1,5 +1,6 @@
 import { Listener, Command } from 'discord-akairo';
 import { MessageEmbed, Message } from 'discord.js';
+import BotClient from '../../client/BotClient';
 
 export default class CommandErrorListener extends Listener {
     public constructor() {
@@ -11,6 +12,12 @@ export default class CommandErrorListener extends Listener {
     }
 
     public async exec(error: Error, message: Message, command: Command | null | undefined) {
-        
+		const errorEmbed: MessageEmbed = new MessageEmbed()
+			.setTitle('A error occured')
+			.setDescription(`**User:** PLACEHOLDER\n**Command:** PLACEHOLDER\n**Channel:** PLACEHOLDER`)
+			.addField('Error', `${await this.client.consts.haste(error.stack)}`)
+			.setColor('#1FD8F1')
+			.setTimestamp();
+		message.util.send(errorEmbed)
 	}
 };
