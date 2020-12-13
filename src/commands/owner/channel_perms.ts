86e5c28080fff06eb91bf1c3934ecaaafd79f4f5
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2020-12-12 21:55:38 -0500

    start channel perm command

---
+++ src/commands/owner/channel_perms.ts
@@ -0,0 +1,20 @@
+import { Command, AkairoError } from 'discord-akairo';
+
+export default class ChannelPermsCommand extends Command {
+    public constructor() {
+        super('channelPerms', {
+            aliases: ['channelPerms', 'channelPerm','cp'],
+            category: 'owner',
+            description: {
+                content: 'A command mass set channel permissions',
+                usage: '',
+                examples: [
+                    ''
+                ]
+            }
+        });
+    };
+    public exec(message: Message) {
+        message.util.send('...')
+    }
+};
