e98bd0e6d1079c18ca881ea49db9b12ff6f2083a
Tyman <=>
2020-12-13 23:18:35 -0500

    a

--- src/config/botoptions.ts
+++
@@ -1,5 +0,0 @@
-export let prefix: string = 'prefix here'    
-export let owners: string[] | string = [                                          
-    'owner id'
-]                                                                                 
-export let errorChannel: string = 'error channel'
