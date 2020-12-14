4261fd6eaf90207a840b467fe53ddffb599ae89a
Tyman <=>
2020-12-14 00:33:13 -0500

    more fixes wow

--- src/commands/owner/channel_perms.ts
+++ src/commands/owner/channel_perms.ts
@@ -1,21 +1,21 @@
-import { Command, AkairoError } from 'discord-akairo';
-import { Message } from 'discord.js';
+import { Command, AkairoError } from 'discord-akairo'
+import { Message } from 'discord.js'
 
 export default class ChannelPermsCommand extends Command {
-    public constructor() {
-        super('channelPerms', {
-            aliases: ['channelPerms', 'channelPerm','cp'],
-            category: 'owner',
-            description: {
-                content: 'A command mass set channel permissions',
-                usage: '',
-                examples: [
-                    ''
-                ]
-            }
-        });
-    };
-    public exec(message: Message) {
-        message.util.send('...')
-    }
-};
+	public constructor() {
+		super('channelPerms', {
+			aliases: ['channelPerms', 'channelPerm','cp'],
+			category: 'owner',
+			description: {
+				content: 'A command mass set channel permissions',
+				usage: '',
+				examples: [
+					''
+				]
+			}
+		})
+	}
+	public exec(message: Message) {
+		message.util.send('...')
+	}
+}
--- src/commands/owner/reload.ts
+++ src/commands/owner/reload.ts
@@ -1,6 +1,6 @@
-import { Command, AkairoError } from 'discord-akairo';
-import { Message } from 'discord.js';
-import { exec } from "child_process";
+import { Command, AkairoError } from 'discord-akairo'
+import { Message } from 'discord.js'
+import { exec } from 'child_process'
 
 export default class ReloadCommand extends Command {
 	public constructor() {
@@ -18,20 +18,20 @@ export default class ReloadCommand extends Command {
 			ratelimit: 4,
 			cooldown: 4000,
 			ownerOnly: true,
-		});
-	};
+		})
+	}
 
 	*args() {
 		const type = yield {
 			id: 'type',
-			type: ["command", "category", "inhibitor", "all"],
+			type: ['command', 'category', 'inhibitor', 'all'],
 			prompt: {
 				start: 'What would you like to reload?',
 				retry: 'Invalid input. What would you like to reload?',
 				time: 30000
 			}
 		}
-		if (type != "all") {
+		if (type != 'all') {
 			const id = yield {
 				id: 'id',
 				type: 'string',
@@ -52,38 +52,38 @@ export default class ReloadCommand extends Command {
 				return message.util.send(`Error recompiling, \`${error.name}\``)
 			}
 			switch (type) {
-				case 'category':
-					try {
-						this.handler.findCategory(id).reloadAll();
-					} catch (e) {
-						return message.util.send(e.message);
-					}
-					break;
-				case 'all':
-					try {
-						this.handler.reloadAll()
-					} catch (e) {
-						return message.util.send(e.message);
-					}
-					break;
-				case 'command':
-					try {
-						this.handler.reload(id)
-					} catch (e) {
-						return message.util.send(e.message);
-					}
-					break;
-				case 'inhibitor':
-					try {
-						this.handler.inhibitorHandler.reload(id)
-					} catch (e) {
-						return message.util.send(e.message);
-					}
-					break;
-				default:
-					return message.util.send("Wtf how did this happen")
+			case 'category':
+				try {
+					this.handler.findCategory(id).reloadAll()
+				} catch (e) {
+					return message.util.send(e.message)
+				}
+				break
+			case 'all':
+				try {
+					this.handler.reloadAll()
+				} catch (e) {
+					return message.util.send(e.message)
+				}
+				break
+			case 'command':
+				try {
+					this.handler.reload(id)
+				} catch (e) {
+					return message.util.send(e.message)
+				}
+				break
+			case 'inhibitor':
+				try {
+					this.handler.inhibitorHandler.reload(id)
+				} catch (e) {
+					return message.util.send(e.message)
+				}
+				break
+			default:
+				return message.util.send('Wtf how did this happen')
 			}
-			message.util.send(`Reloaded ${(id == undefined && type == "all") ? "all" : id} 🔁`)
+			message.util.send(`Reloaded ${(id == undefined && type == 'all') ? 'all' : id} 🔁`)
 		})
-	};
-};
+	}
+}
