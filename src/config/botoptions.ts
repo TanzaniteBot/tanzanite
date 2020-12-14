3f42cabb29885f589e010c3da836c747576e86bb
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2020-12-13 20:42:22 -0700

    Add placeholder config

---
+++ src/config/botoptions.ts
@@ -0,0 +1,5 @@
+export let prefix: string = 'prefix here'    
+export let owners: string[] | string = [                                          
+    'owner id:
+]                                                                                 
+export let errorChannel: string = 'error channel'
