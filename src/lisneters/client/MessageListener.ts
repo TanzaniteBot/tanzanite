ac5a7b7ad4f8af5c2b2069b81419dd2d227dfde2
TrashCan <samuel2007davis@gmail.com>
2020-12-14 23:25:30 -0500

    ok tymon

--- src/lisneters/client/MessageListener.ts
+++ src/lisneters/client/MessageListener.ts
@@ -1,6 +1,7 @@
 import { Listener } from 'discord-akairo'
 import { Message, MessageEmbed, TextChannel } from 'discord.js'
 
+
 export default class ReadyListener extends Listener {
 	public constructor() {
 		super('MessageListener', {
@@ -14,10 +15,14 @@ export default class ReadyListener extends Listener {
 		// on dm
 		if(message.channel.type === 'dm') {
 			if (message.author.bot) return
+			if (message.util?.parsed?.command) return
 			// add a thing to send it so a specific channel
 			const dmlogembed = new MessageEmbed()
 				.setAuthor(`From: ${message.author.username}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
 				.setDescription(`**DM:**\n${message}`)
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				.setColor(this.client.consts.DefaultColor)
 				.setTimestamp()
 				.setFooter(`ID • ${message.author.id}`)
 			const dmchannel = <TextChannel> this.client.channels.cache.get('783129374551572512') // temp hardcoded, will fix later
--- src/lisneters/commands/CommandErrorListener.ts
+++ src/lisneters/commands/CommandErrorListener.ts
@@ -18,19 +18,19 @@ export default class CommandErrorListener extends Listener {
 		const errorChannel = <TextChannel> client.channels.cache.get(client.config.errorChannel)
 		const errorNo = Math.floor(Math.random() * 6969696969) + 69 // hehe funy number
 		const errorEmbed: MessageEmbed = new MessageEmbed()
-			.setTitle(`Error no \`${errorNo}\`: An error occurred`)
+			.setTitle(`Error # \`${errorNo}\`: An error occurred`)
 			.setDescription(
 				stripIndents`**User:** ${message.author} (${message.author.tag})
 				**Command:** ${command}
 				**Channel:** ${message.channel} (${message.channel.id})
-				**Message:** ${message.url}`
+				**Message:** [link](${message.url})`
 			)
 			.addField('Error', `${await client.consts.haste(error.stack)}`)
 			.setColor(client.consts.ErrorColor)
 			.setTimestamp()
 		const errorUserEmbed: MessageEmbed = new MessageEmbed()
 			.setTitle('An error occurred')
-			.setDescription(`Oh no! While running the command \`${command.aliases[0]}\`, an error happened. Please give the developers code ${errorNo}.`)
+			.setDescription(`Oh no! While running the command \`${command.aliases[0]}\`, an error happened. Please give the developers code \`${errorNo}\`.`)
 			.setColor(client.consts.ErrorColor)
 			.setTimestamp()
 		errorChannel.send(errorEmbed)
