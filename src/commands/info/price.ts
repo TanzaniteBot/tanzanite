5f3e0f232569a6b697f4f98c297e61fc4df36cdf
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2020-12-26 18:40:35 -0500

    Removed my comment crap

--- src/commands/info/price.ts
+++ src/commands/info/price.ts
@@ -29,8 +29,6 @@ export default class PriceCommand extends Command {
 		})
 	}
 	public async exec(message: Message, { item }: { item: Command }): Promise<void> {
-		
-
 		const price = JSON.parse((await got.get('http://51.75.78.252/lowestbin.json')).body)
 		const itemstring = item.toString() 
 		if (item === undefined) {
@@ -39,30 +37,11 @@ export default class PriceCommand extends Command {
 		}
 		
 		try {
-			/*const test = JSON.stringify((await got.get('http://51.75.78.252/lowestbin.json')).body)
-			const test1 = test.substring(1, 2000)
-			const test2 = test.match
-			message.channel.send(test1)
-			message.channel.send(test2)
-			
-
-			JSON.stringify(test)
-			*/
-
 			message.channel.send(price[itemstring])
 
 
 		} catch(e) {
 			message.channel.send('error')
 		}
-		
-
-		/*message.channel.send('.' + item)*/
-		
-
 	}
-/**     
-	public async exec(message: Message, { command }: { command: Command }): Promise<Message | Message[]> {
-
-	} */
 }
