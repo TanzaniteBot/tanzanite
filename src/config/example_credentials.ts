24d4b82405a3fadf3f56d1793abfb14d816b9594
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-01-02 15:41:48 -0500

    commit message

--- src/config/example_credentials.ts
+++ src/config/example_credentials.ts
@@ -1,2 +1,2 @@
-// remove the "example_" from the file name and replace rhe bot token value to make the bot work
+// remove the "example_" from the file name and replace the bot token value to make the bot work
 export const token = 'PUT TOKEN HERE'
