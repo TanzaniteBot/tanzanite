07757eb241a78936818d43ee1fd517dffdc6069b
Tyman <=>
2021-01-07 13:51:31 -0500

    why do we need those comments anyway

--- src/commands/info/serverstats.ts
+++ src/commands/info/serverstats.ts
@@ -20,43 +20,7 @@ export default class ServerStatusCommand extends BotCommand {
 		})
 	}
 	public async exec(message: Message): Promise<void> {
-		/*const msgEmbed: MessageEmbed = new MessageEmbed()
-			.setTitle('Server status')
-			.setDescription('Checking servers:\nMain: ...\nBackup: ...')
-			.setColor(this.client.consts.DefaultColor)
-		const msg: Message = await message.util.send(msgEmbed)
-		let main, back: string
-		try {
-			JSON.parse((await got.get('http://51.79.51.21/lowestbin.json')).body)
-			main = '✅'
-		} catch (e) {
-			main = '❌'
-		}
-
-		await msg.edit(msgEmbed.setDescription(`Checking servers:\nMain: ${main}\nBackup: ...`))
-
-		try {
-			JSON.parse((await got.get('http://51.75.78.252/lowestbin.json')).body)
-			back = '✅'
-		} catch (e) {
-			back = '❌'
-		}
-		await msg.edit(msgEmbed.setDescription(`Checking servers:\nMain: ${main}\nBackup: ${back}`))
-
-		await this.client.consts.sleep(0.5)
-
-		if ((back == '✅' && main == '❌') || (main == '✅' && back == '❌')) {
-			await msg.edit(msgEmbed.addField('Status', 'It appears one of the servers was online, this means that it should be fine as long as you have the latest version of NEU.').setColor(this.client.consts.Orange))
-		}
-		else if (back == '❌' && main == '❌') {
-			await msg.edit(msgEmbed.addField('Status', 'It appears both of the servers are offline, this means that everything related to prices will likely not work.').setColor(this.client.consts.Red))
-		}
-		else {
-			await msg.edit(msgEmbed.addField('Status', 'Both of the servers are online, all features related to prices will likely work.').setColor(this.client.consts.Green))
-		}*/
-
-
-		//The servers seem to have changed ips
+		
 		const msgEmbed: MessageEmbed = new MessageEmbed()
 			.setTitle('Server status')
 			.setDescription('Checking servers:\nMain: ...\nBackup: ...')
