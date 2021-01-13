8d36db7f81e10702cdf02400c55f606b568e99f8
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2021-01-13 11:39:24 -0700

    e

--- src/constants/functions.ts
+++ src/constants/functions.ts
@@ -185,10 +185,20 @@ async function discordLog(logMessage: string): Promise<void> {
 	await generalLogChannel.send(logMessage)
 }
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 export = {
 	haste,
 	paginate,
 	sleep,
 	resolveMentions,
-	discordLog
+	discordLog,
+	getRandomColor
 }
