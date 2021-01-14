d41f6aa0c0c82c7a3d68fb862dbcb940afa1f052
Tyman <=>
2021-01-14 16:20:35 -0500

    bruh

--- src/listeners/message/ContentCreatorListener.ts
+++ src/listeners/message/ContentCreatorListener.ts
@@ -13,26 +13,22 @@ export default class ContentCreatorListener extends BotListener {
 	public async exec(message: Message): Promise<void> {
 		if (!message.guild) return
 		const generalLogChannel = <TextChannel> this.client.channels.cache.get(this.client.config.generalLogChannel)
-		try {
-			if(message.mentions.members.first()?.roles.cache.has('729414120842985564')) {
-				if(message.mentions.members.first()?.roles.cache.has('737308259823910992')) return
-				if(message.mentions.members.first()?.roles.cache.has('782803470205190164')) return
-				if(message.author.bot) return
-				await message.reply('Please dont mention content creators');
-				const mentionlogembed = new MessageEmbed()
-					.setTitle('A content creator was mentioned')
-					.setColor(this.client.consts.DefaultColor)
-					.addField('Mentioned User', `${message.mentions.members.first()}`)
-					.addField('User', `${message.author} **|** ${message.author.id}`, false)
-					.addField('Msg', `${message.channel}(**[link](${message.url})**)\n\n**Contents:** ${message}`)
-					.setTimestamp()
-					.setFooter('OwO')
-				await generalLogChannel.send(mentionlogembed)
-			} else {
-				return
-			}
-		} catch (error) {
-			generalLogChannel.send(error)
+		if(message.mentions.members.first()?.roles.cache.has('729414120842985564')) {
+			if(message.mentions.members.first()?.roles.cache.has('737308259823910992')) return
+			if(message.mentions.members.first()?.roles.cache.has('782803470205190164')) return
+			if(message.author.bot) return
+			await message.reply('Please dont mention content creators');
+			const mentionlogembed = new MessageEmbed()
+				.setTitle('A content creator was mentioned')
+				.setColor(this.client.consts.DefaultColor)
+				.addField('Mentioned User', `${message.mentions.members.first()}`)
+				.addField('User', `${message.author} **|** ${message.author.id}`, false)
+				.addField('Msg', `${message.channel}(**[link](${message.url})**)\n\n**Contents:** ${message}`)
+				.setTimestamp()
+				.setFooter('OwO')
+			await generalLogChannel.send(mentionlogembed)
+		} else {
+			return
 		}
 		
 	}
