e187c0202de73249251aecd2d9c199c674f24e8c
TymanWasTaken <tymanwastaken@users.noreply.github.com>
2021-01-13 20:44:39 -0700

    Split MessageListener.ts into message listener category

---
+++ src/listeners/message/AutopublisherListener.ts
@@ -0,0 +1,25 @@
+import {BotListener} from '../../classes/BotListener';
+import {Message, TextChannel} from 'discord.js';
+
+export default class MessageListener extends BotListener {
+	public constructor() {
+		super('MessageListener', {
+			emitter: 'client',
+			event: 'message',
+			category: 'message'
+		})
+	}
+
+	public async exec(message: Message): Promise<void> {
+		if(message.channel.type === 'news' && this.client.config.autoPublishChannels.some(x => message.channel.id.includes(x))){
+			const generalLogChannel = <TextChannel> this.client.channels.cache.get(this.client.config.generalLogChannel)
+			try{
+				await generalLogChannel.send(`Found unpublished message (<${message.url}>) in channel ${message.channel.name}(${message.channel.id}) in ${message.guild.name}`)
+				await message.crosspost()
+				await generalLogChannel.send('Published message.')
+			}catch(e){
+				await generalLogChannel.send(e)
+			}
+		}
+	}
+}
