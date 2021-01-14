a2e0db785218c2333e213b7ac156a2f30c656bb5
Tyman <=>
2021-01-14 16:27:24 -0500

    funy

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -18,12 +18,12 @@ export default class ReloadCommand extends BotCommand {
 		})
 	}
 	public async exec(message: Message): Promise<void> {
-		const replyMsg: Message = await message.util.send('Pong!')
+		const replyMsg: Message = await message.util.send(`${(message.util.parsed.alias == 'ping') ? 'Ping' : 'Pong'}?`)
 		const timestamp: number = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp
 		const latency = `\`\`\`\n ${Math.floor(replyMsg.createdTimestamp - timestamp)}ms \`\`\``
 		const apiLatency = `\`\`\`\n ${Math.round(message.client.ws.ping)}ms \`\`\``
 		const embed: MessageEmbed = new MessageEmbed()
-			.setTitle('Pong!  🏓')
+			.setTitle(`${(message.util.parsed.alias == 'ping') ? 'pong' : 'ping'}!  🏓`)
 			.addField('Latency', latency, true)
 			.addField('API Latency', apiLatency, true)
 			.setFooter(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
