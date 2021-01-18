6b0ea80a95e21a53152d1fb2befc5cc652d64ac0
TrashCan69420 <arbuclejhon07@gmail.com>
2021-01-18 12:30:53 -0500

    ahhh i keep finding \n

--- src/commands/tags/install.ts
+++ src/commands/tags/install.ts
@@ -23,7 +23,8 @@ export default class InstallCommand extends BotCommand {
 					__**Installation Instructions**__
 					You will need to install forge to use Minecraft mods, see below on how to do so:
 					1) Run normal Minecraft 1.8.9 and once it reaches the title screen wait about 5 seconds and close it.
-					2) Install Minecraft **1.8.9** forge from the website here\n- Once you click on the installed you'd like to download, a window will pop up.** Do not click on anything in the middle of your screen**; instead, click on the \`skip ad\` button towards the top right
+					2) Install Minecraft **1.8.9** forge from the website here
+					- Once you click on the installed you'd like to download, a window will pop up.** Do not click on anything in the middle of your screen**; instead, click on the \`skip ad\` button towards the top right
 					3): Open the installer, select install client, and click install
 					4): When forge is installed, open the Minecraft launcher, go under the \`installations tab\`, click \`new installation\`, select the version release \`1.8.9-forge1.8.9-11.15.1.xxxx\` (it will usually be all the way towards the bottom).
 					5) Once you are done, run this new installation that you just created. Once it reaches the title screen, wait about 5 seconds and close it.
@@ -35,7 +36,8 @@ export default class InstallCommand extends BotCommand {
 					- If you have java installed, double click the file, click \`install\`
 					- If not, press the windows key + R; type \`%appdata%\`; click on the folder called \`.minecraft\`; click on the folder called \`mods\` and drag the mods file in here.
 					4): Open the Minecraft launcher and run your forge installation you set up earlier. 
-					5): Then, hop onto Skyblock and run the command /api new. Your api key is automatically filled out and all features should work.\n6): Type \`/neu\`. If you see the NotEnoughUpdates menu, you have done this correctly!
+					5): Then, hop onto Skyblock and run the command /api new. Your api key is automatically filled out and all features should work.
+					6): Type \`/neu\`. If you see the NotEnoughUpdates menu, you have done this correctly!
 					
 					
 					If you need any more assistance after following this guide, please send a message in <#714332750156660756> or <#737414807250272258> wait patiently to be helped.`
