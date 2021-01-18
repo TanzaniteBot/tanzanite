8b2bd1ee73fcd7f125900dc92d60541ede477f2b
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2021-01-18 09:56:01 -0700

    Update functions.ts

--- src/constants/functions.ts
+++ src/constants/functions.ts
@@ -23,8 +23,24 @@ async function reactAll(m: Message, ...relations: string[]) {
 }
 
 async function haste(content: string): Promise<string> {
-	const res: hastebinRes = await got.post('https://hastebin.com/documents', { body: content }).json();
-	return 'https://hst.sh/' + res.key;
+	const urls = [
+		"https://hst.sh",
+		"https://hasteb.in",
+		"https://hastebin.com",
+    		"https://mystb.in",
+    		"https://haste.clicksminuteper.net",
+    		"https://paste.pythondiscord.com",
+    		"https://haste.unbelievaboat.com"		
+	]
+	for (const url of urls) {
+		try {
+			const res: hastebinRes = await got.post(`${url}/documents`, { body: content }).json()
+			return url + '/' + res.key
+		} catch (e) {
+			continue
+		}
+	}
+	return "Unable to post"
 }
 
 async function paginate(message: Message, embeds: MessageEmbed[]): Promise<void> {
