9c4a3849e86dd3f8c8b32514d428f7babd9e31de
TrashCan69420 <arbuclejhon07@gmail.com>
2021-01-18 15:47:52 -0500

    started guild info

--- src/commands/info/guildinfo.ts
+++ src/commands/info/guildinfo.ts
@@ -1,1 +1,24 @@
-//soontm
+import { BotCommand } from "../../extensions/BotCommand";
+import { Message } from 'discord.js'
+
+export default class GuildInfoCommand extends BotCommand {
+    public constructor() {
+    super('guildinfo', {
+            aliases: ['guildinfo', 'serverinfo'],
+            category: 'info',
+            ratelimit: 4,
+			cooldown: 4000,
+            description: {
+                content: 'Use to get info about the server the command was run in',
+                usage: 'severinfo',
+                examples: ['serverinfo']
+            },
+			channel: 'guild',
+			clientPermissions: ['EMBED_LINKS'],
+
+        })
+    }
+    public exec(message: Message) {
+        message.channel.send('wip');
+    }
+}
