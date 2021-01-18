8661e3e932e51ffa286b5b5f614d9a9e7f6994a7
TrashCan69420 <arbuclejhon07@gmail.com>
2021-01-18 17:59:15 -0500

    flags

--- src/constants/flags.ts
+++ src/constants/flags.ts
@@ -1,1 +1,12 @@
-// flags for guild info command for the region shit i cba to add them rn
+export = {
+    brazil : ':flag_br:',
+    europe : ':flag_eu:',
+    HongKong : ':flag_hk:',
+    india : ':flag_in:',
+    japan : ':flag_jp:',
+    russia : ':flag_ru:',
+    signapore : ':flag_sg:', // scuffed spelling(probably for more than this one too) ill fix it later
+    south_africa : ':flag_ss:',
+    sydney : ':flag_au:',
+    us : ':flag_us:'
+}
