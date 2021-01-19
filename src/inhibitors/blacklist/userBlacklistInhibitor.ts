bae34cef5ab4a8cee9ab9e531b9bea7cf1d331d7
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2021-01-18 21:49:41 -0700

    hm

--- src/inhibitors/blacklist/userBlacklistInhibitor.ts
+++ src/inhibitors/blacklist/userBlacklistInhibitor.ts
@@ -9,7 +9,7 @@ export default class UserBlacklistInhibitor extends BotInhibitor {
 	}
 
 	public exec(message: Message): boolean {
-		if (!this.client.config.owners.includes(message.author.id) || !this.client.config.superUsers.includes(message.author.id)) {
+		if (!(this.client.config.owners.includes(message.author.id) || this.client.config.superUsers.includes(message.author.id))) {
 			if (this.client.config.userBlacklist.includes(message.author.id)) {
 				message.react('<:mad:783046135392239626>');
 				return true;
