e23caf7e09d6722ee06d29020f35b709b8d0059d
TrashCan69420 <arbuclejhon07@gmail.com>
2021-01-19 12:48:37 -0500

    auto publish embed thing

--- src/listeners/message/AutopublisherListener.ts
+++ src/listeners/message/AutopublisherListener.ts
@@ -1,5 +1,5 @@
+import { Message, TextChannel, MessageEmbed } from 'discord.js';
 import { BotListener } from '../../extensions/BotListener';
-import { Message, TextChannel } from 'discord.js';
 
 export default class APListener extends BotListener {
 	public constructor() {
@@ -11,14 +11,24 @@ export default class APListener extends BotListener {
 	}
 
 	public async exec(message: Message): Promise<void> {
+		const PublishEmbed = new MessageEmbed()
+			.setTitle('Found an unpublished message')
+			.addField('MSG Link', `[link](${message.url})`, false)
+			.addField('Channel', `<#${message.channel.id}>`, false)
+			.setColor(this.client.consts.Red)
+			.setFooter(`${message.guild.id}`, message.guild.iconURL())
+			.setTimestamp();
+
+		const generalLogChannel = <TextChannel>this.client.channels.cache.get(this.client.config.generalLogChannel);
+
 		if (message.channel.type === 'news' && this.client.config.autoPublishChannels.some((x) => message.channel.id.includes(x))) {
-			const generalLogChannel = <TextChannel>this.client.channels.cache.get(this.client.config.generalLogChannel);
 			try {
-				await generalLogChannel.send(
-					`Found unpublished message (<${message.url}>) in channel ${message.channel.name}(${message.channel.id}) in ${message.guild.name}`
-				);
 				await message.crosspost();
-				await generalLogChannel.send('Published message.');
+				generalLogChannel.send(PublishEmbed).then((msg) => {
+					PublishEmbed.setTitle('Published a message');
+					PublishEmbed.setColor(this.client.consts.Green);
+					msg.edit(PublishEmbed);
+				});
 			} catch (e) {
 				await generalLogChannel.send(e);
 			}
