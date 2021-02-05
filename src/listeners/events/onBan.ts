a5d31897cb4eabcaf275fed28d939854c97a3ed2
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-02-05 16:35:39 -0500

    .

--- src/listeners/events/onBan.ts
+++ src/listeners/events/onBan.ts
@@ -1,6 +1,5 @@
-import { TextChannel               } from 'discord.js'                  ;
-import { GuildMember, MessageEmbed } from 'discord.js'                  ;
-import { BotListener               } from '../../extensions/BotListener';
+import { TextChannel, Guild, GuildMember, MessageEmbed } from 'discord.js'                   ;
+import { BotListener                                   } from '../../extensions/BotListener' ;
 
 export default class OnBanListener extends BotListener {
 	public constructor() {
@@ -11,9 +10,9 @@ export default class OnBanListener extends BotListener {
 		});
 	}
 
-	public async exec(member: GuildMember): Promise<void> {
-		if (member.guild.id == '516977525906341928'){
-			//console.log(member.roles.cache)
+	public async exec(member: GuildMember, guild: Guild): Promise<void> {
+		if (guild.id == '516977525906341928'){
+			//This is where I would put the member's cached roles in the database  
 			const welcome = <TextChannel>this.client.channels.cache.get('737460457375268896')			
 			const embed: MessageEmbed = new MessageEmbed()
 				.setDescription(`A member was banned from the server. There are now ${welcome.guild.memberCount.toLocaleString()} members.`)
