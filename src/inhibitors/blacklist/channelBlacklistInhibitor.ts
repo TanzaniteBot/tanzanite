2f086b69f079b7235d9034d61b5365df8f09b3b7
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-02-09 23:15:33 -0500

    vyvycyfyfycygyct

--- src/inhibitors/blacklist/channelBlacklistInhibitor.ts
+++ src/inhibitors/blacklist/channelBlacklistInhibitor.ts
@@ -10,7 +10,11 @@ export default class ChannelBlacklistInhibitor extends BotInhibitor {
 	}
 
 	public exec(message: Message): boolean {
-		if ((this.client.ownerID.includes(message.author.id) != true) ||(this.client.config.superUsers.includes(message.author.id) != true)/*||this.client.config.roleWhitelist.includes(message.author.id) (message.member.roles.cache.some(r => this.client.config.roleWhitelist.includes(r.id)) != true)*/) {
+		if (
+			!(this.client.ownerID.includes(message.author.id) ||
+			this.client.config.superUsers.includes(message.author.id))
+			/*||!message.member.roles.cache.some(r => this.client.config.roleWhitelist.includes(r.id))*/ // why is this commented out?
+		) {
 			if (this.client.config.channelBlacklist.includes(message.channel.id)) {
 				message.react(this.client.consts.mad);
 				return true;
