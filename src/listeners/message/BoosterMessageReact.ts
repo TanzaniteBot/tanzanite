a068a885aae3fe12a1c406ce3e28bb4eab438980
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-02-17 13:17:03 -0500

    Add nitroboost emoji when somone boosts

---
+++ src/listeners/message/BoosterMessageReact.ts
@@ -0,0 +1,23 @@
+import { Message } from 'discord.js';
+import { BotListener } from '../../extensions/BotListener';
+
+export default class AutoResponderListener extends BotListener {
+	public constructor() {
+		super('AutoResponderListener', {
+			emitter: 'client',
+			event: 'message',
+			category: 'message',
+		});
+	}
+	public async exec(message: Message): Promise<void> {
+		if ((message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION')&&(message.guild.id === '516977525906341928')&&(message.channel.id === '784479510056665138')){
+			try{ 
+				message.react('<:nitroboost:785160348885975062>')
+			}catch{
+				//
+			}
+			
+		}
+		
+	}
+}
