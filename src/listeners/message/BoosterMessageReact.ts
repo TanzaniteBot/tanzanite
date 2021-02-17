c0568519a6615f756ea41c7f54552fd1708d9dab
TrashCan <66451576+TrashCan69420@users.noreply.github.com>
2021-02-17 13:43:03 -0500

    so tyman no yell at me
    so tyman no yell at me

--- src/listeners/message/BoosterMessageReact.ts
+++ src/listeners/message/BoosterMessageReact.ts
@@ -9,7 +9,7 @@ export default class AutoResponderListener extends BotListener {
 			category: 'message',
 		});
 	}
-	public async exec(message: Message) {
+	public async exec(message: Message): Promise<Message> {
 		if ((message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION')&&(message.guild.id === '516977525906341928')&&(message.channel.id === '784479510056665138')){
 			try{ 
 				message.react('<:nitroboost:785160348885975062>')
