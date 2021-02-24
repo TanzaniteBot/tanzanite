465cdd184981cabdd8ded15e91a4b16df70ead3d
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-02-24 13:32:29 -0700

    fix

--- src/config/example_credentials.ts
+++
@@ -1,5 +0,0 @@
-// remove the "example_" from the file name and replace the values
-export const 
-	token = 'PUT TOKEN HERE',	
-	MongoDB = 'PUT MONGODB LOGIN HERE',
-	hypixelApiKey = 'PUT API KEY HERE';
--- src/listeners/message/BlacklistedFileListener.ts
+++ src/listeners/message/BlacklistedFileListener.ts
@@ -5,10 +5,16 @@ import got from 'got';
 import crypto from 'crypto';
 
 export default class BlacklistedFileListener extends BotListener {
-	private blacklistedFiles = {
-		'a0f5e30426234bc9d09306ffc9474422': 'Play twice audio',
-		'7a0831239e8c8368e96fb4cacd61b5f2': 'Web/Desktop crash video'
-	}
+	private blacklistedFiles = [
+		{
+			hash: 'a0f5e30426234bc9d09306ffc9474422',
+			name: 'Play twice audio'
+		},
+		{
+			hash: '7a0831239e8c8368e96fb4cacd61b5f2',
+			name: 'Web/Desktop crash video'
+		}
+	]
 	constructor() {
 		super('blacklistedFiles', {
 			event: 'message',
@@ -18,17 +24,22 @@ export default class BlacklistedFileListener extends BotListener {
 	}
 	public async exec(message: Message): Promise<void> {
 		if (message.attachments.size < 1) return
-		const foundFiles = []
+		const foundFiles = [] as {name: string; hash: string}[]
 		for (const attachment of message.attachments) {
-			const req = await got.get(attachment[1].proxyURL)
-			const rawHash = crypto.createHash('md5')
-			rawHash.update(req.rawBody.toString('binary'))
-			const hash = rawHash.digest('hex')
-			if (hash in this.blacklistedFiles) {
-				foundFiles.push(this.blacklistedFiles[hash])
+			try {
+				const req = await got.get(attachment[1].proxyURL)
+				const rawHash = crypto.createHash('md5')
+				rawHash.update(req.rawBody.toString('binary'))
+				const hash = rawHash.digest('hex')
+				const blacklistData = this.blacklistedFiles.find(h => h.hash === hash)
+				if (blacklistData !== undefined) {
+					foundFiles.push(blacklistData)
+				}
+			} catch {
+				continue
 			}
 		}
-		if (foundFiles.length < 1) {
+		if (foundFiles.length > 0) {
 			try {
 				await message.delete()
 				await this.client.log({
@@ -37,7 +48,7 @@ export default class BlacklistedFileListener extends BotListener {
 						description: stripIndent`
 							Blacklisted ${foundFiles.length === 1 ? 'file was' : 'files were'} deleted in ${message.channel}.
 							Author: <@!${message.author.id}> (${message.author.tag})
-							Files found: ${foundFiles.reduce((p, c, i) => i == foundFiles.length - 1 ? `${p}, and ${c}` : `${p}, ${c}`)}
+							Files found: ${foundFiles.map(f => `${f.name} (${f.hash})`).reduce((p, c, i) => i == foundFiles.length - 1 ? `${p}, and ${c}` : `${p}, ${c}`)}
 						`
 					}
 				})
@@ -48,7 +59,7 @@ export default class BlacklistedFileListener extends BotListener {
 						description: stripIndent`
 							Blacklisted ${foundFiles.length === 1 ? 'file was' : 'files were'} sent in ${message.channel}.
 							Author: <@!${message.author.id}> (${message.author.tag})
-							Files found: ${foundFiles.reduce((p, c, i) => i == foundFiles.length - 1 ? `${p}, and ${c}` : `${p}, ${c}`)}
+							Files found: ${foundFiles.map(f => `${f.name} (${f.hash})`).reduce((p, c, i) => i == foundFiles.length - 1 ? `${p}, and ${c}` : `${p}, ${c}`)}
 
 							Unable to delete file, an error occurred.
 						`
