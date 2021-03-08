fd63e0b9a796ed63bb146418a5c9b677d984d96a
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-03-07 18:44:53 -0700

    bruh

--- src/commands/info/pronouns.ts
+++ src/commands/info/pronouns.ts
@@ -2,29 +2,31 @@ import { User, Message, MessageEmbed } from 'discord.js';
 import { BotCommand } from '../../extensions/BotCommand';
 import got, { HTTPError } from 'got';
 
-export type pronounsType =
-	| 'unspecified'
-	| 'hh'
-	| 'hi'
-	| 'hs'
-	| 'ht'
-	| 'ih'
-	| 'ii'
-	| 'is'
-	| 'it'
-	| 'shh'
-	| 'sh'
-	| 'si'
-	| 'st'
-	| 'th'
-	| 'ti'
-	| 'ts'
-	| 'tt'
-	| 'any'
-	| 'other'
-	| 'ask'
-	| 'avoid';
+export const pronounMapping = {
+	unspecified: 'Unspecified',
+	hh: 'He/Him',
+	hi: 'He/It',
+	hs: 'He/She',
+	ht: 'He/They',
+	ih: 'It/Him',
+	ii: 'It/Its',
+	is: 'It/She',
+	it: 'It/They',
+	shh: 'She/He',
+	sh: 'She/Her',
+	si: 'She/It',
+	st: 'She/They',
+	th: 'They/He',
+	ti: 'They/It',
+	ts: 'They/She',
+	tt: 'They/Them',
+	any: 'Any pronouns',
+	other: 'Other pronouns',
+	ask: 'Ask me my pronouns',
+	avoid: 'Avoid pronouns, use my name'
+}
 
+export type pronounsType = keyof typeof pronounMapping;
 export default class PronounsCommand extends BotCommand {
 	constructor() {
 		super('pronouns', {
@@ -41,10 +43,11 @@ export default class PronounsCommand extends BotCommand {
 	async exec(message: Message, { user }: { user: User | null }): Promise<void> {
 		if (user === null) user = message.author;
 		try {
-			const pronouns: { pronouns: pronounsType } = await got.get(`https://pronoundb.org/api/v1/lookup?platform=discord&id=${user.id}`).json();
+			const apiRes: { pronouns: pronounsType } = await got.get(`https://pronoundb.org/api/v1/lookup?platform=discord&id=${user.id}`).json();
 			await message.util.reply(
 				new MessageEmbed({
 					title: `${user.tag}'s pronouns:`,
+					description: pronounMapping[apiRes.pronouns]
 				})
 			);
 		} catch (e) {
