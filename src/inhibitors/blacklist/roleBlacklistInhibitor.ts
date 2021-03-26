cd4c15c13eb0f4b2a004b0fa650eaa93252452f2
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-03-25 22:34:10 -0400

    frick now stfu

--- src/inhibitors/blacklist/roleBlacklistInhibitor.ts
+++ src/inhibitors/blacklist/roleBlacklistInhibitor.ts
@@ -16,7 +16,7 @@ export default class RoleBlacklistInhibitor extends BushInhibitor {
 			roleBlacklist: string[] = (await db.globalGet('roleBlacklist', [])) as string[];
 		if (!(this.client.config.owners.includes(message.author.id) || superUsers.includes(message.author.id))) {
 			if (message.guild) {
-				if (message.member.roles?.cache.some(r => roleBlacklist.includes(r.id))) {
+				if (message.member?.roles?.cache.some(r => roleBlacklist.includes(r.id))) {
 					message.react(this.client.consts.mad).catch(e => {
 						log.debug(e);
 					});
