6ca872a95b91e0f36cf50705f777696f95e280cb
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-03-25 22:16:25 -0400

    added cool down indication

---
+++ src/commands/dev/channelbypass.ts
@@ -0,0 +1,34 @@
+import { BushCommand, PermissionLevel } from '../../lib/extensions/BushCommand';
+import { Message, User } from 'discord.js';
+import db from '../../constants/db';
+
+export default class ChannelBypassCommand extends BushCommand {
+	public constructor() {
+		super('channelbypass', {
+			aliases: ['channelbypass', 'bypasschannel'],
+			category: 'dev',
+			description: {
+				content: 'A command to add people that bypass the channel blacklist.',
+				usage: 'superuser <user>',
+				examples: ['superuser IRONM00N']
+			},
+			args: [
+				{
+					id: 'user',
+					type: 'user',
+					match: 'content',
+					prompt: {
+						start: 'What user would you like to change the channel bypass status of?',
+						retry: '<:no:787549684196704257> Choose a valid user.'
+					}
+				}
+			],
+			permissionLevel: PermissionLevel.Owner,
+			clientPermissions: ['SEND_MESSAGES'],
+			hidden: true
+		});
+	}
+	public async exec(message: Message, { user }: { user: User }): Promise<void> {
+		//
+	}
+}
---
+++ src/listeners/commands/CommandCooldownListener.ts
@@ -0,0 +1,20 @@
+import { Message } from 'discord.js';
+import { BushListener } from '../../lib/extensions/BushListener';
+import { Command } from 'discord-akairo';
+import log from '../../lib/utils/log';
+
+export default class CommandCooldownListener extends BushListener {
+	public constructor() {
+		super('commandError', {
+			emitter: 'commandHandler',
+			event: 'cooldown',
+			category: 'commands'
+		});
+	}
+
+	public async exec(message: Message, command: Command | null | undefined, remaining: number): Promise<void> {
+		log.info('CommandCooldown', `<<${message.author.tag}>> tried to run <<${message.util.parsed.command}>> but it is on cooldown for <<${remaining/1000}>> seconds.`);
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		await message.react(':hourglass_flowing_sand:').catch(()=>{})
+	}
+}
