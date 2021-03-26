f0f34f52c9dad55de647496941fcb47749b8a9b4
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-03-25 22:31:36 -0400

    a

--- src/inhibitors/blacklist/roleBlacklistInhibitor.ts
+++ src/inhibitors/blacklist/roleBlacklistInhibitor.ts
@@ -1,6 +1,7 @@
 import { BushInhibitor } from '../../lib/extensions/BushInhibitor';
 import { Message } from 'discord.js';
 import db from '../../constants/db';
+import log from '../../lib/utils/log';
 
 export default class RoleBlacklistInhibitor extends BushInhibitor {
 	constructor() {
@@ -16,7 +17,7 @@ export default class RoleBlacklistInhibitor extends BushInhibitor {
 		if (!(this.client.config.owners.includes(message.author.id) || superUsers.includes(message.author.id))) {
 			if (message.guild) {
 				if (message.member.roles?.cache.some(r => roleBlacklist.includes(r.id))) {
-					message.react(this.client.consts.mad);
+					message.react(this.client.consts.mad).catch((e)=>{log.debug(e)})
 					return true;
 				} else {
 					return false;
