012e4715426c31b3d8c66e67d3b1417d9ea8b028
IRONM00N <IRONM00N@users.noreply.github.com>
2021-03-29 12:38:00 -0400

    aa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
 			name: 'BushBot',
 			script: 'yarn',
 			args: 'start',
-			max_memory_restart: '1G',
-			node_args: ['--max_old_space_size=2000'],
+			max_memory_restart: '2000M',
+			node_args: ['--max_old_space_size=2048'],
 			env: {
 				FORCE_COLOR: '3'
 			}
