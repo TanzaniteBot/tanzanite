a37c4aaf7109bd86efe76cd848607f5f0acc3863
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-03-30 20:57:18 -0400

    ty

--- src/commands/Moulberry Bush/role.ts
+++ src/commands/Moulberry Bush/role.ts
@@ -121,7 +121,7 @@ export default class RoleCommand extends BushCommand {
 				);
 				return;
 			}
-			if (message.member.roles.cache.has(role.id)) {
+			if (message.member.roles.cache.has(role.id)) { //someone fix plz
 				log.debug('Need to remove role.');
 				// eslint-disable-next-line @typescript-eslint/no-empty-function
 				const success = await user.roles.remove(role.id).catch(() => {});
