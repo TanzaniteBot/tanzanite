5016c853f2bf042a621fb085e408da05c7fb7411
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-04-07 09:06:07 -0400

    added return roles command

---
+++ src/commands/Moulberry Bush/returnroles.ts
@@ -0,0 +1,50 @@
+import { BushCommand } from '../../lib/extensions/BushCommand';
+import AllowedMentions from '../../lib/utils/AllowedMentions';
+import { Message, MessageEmbed } from 'discord.js';
+import { BushGuildMember } from '../../lib/extensions/BushGuildMember';
+import { stickyRoleDataSchema } from '../../lib/utils/mongoose';
+
+export default class ReturnRolesCommand extends BushCommand {
+	public constructor() {
+		super('returnroles', {
+			aliases: ['returnroles', 'returnrole'],
+			category: "Moulberry's Bush",
+			description: {
+				content: 'A command to return sticky roles manually.',
+				usage: 'returnroles <user>',
+				examples: ['returnroles Bestower']
+			},
+			args: [
+				{
+					id: 'member',
+					type: 'member',
+					prompt: {
+						start: 'What user would you like to return the roles of?',
+						retry: '<:no:787549684196704257> Choose a valid user to return the roles of.',
+						optional: true
+					},
+					default: undefined
+				}
+			],
+			clientPermissions: ['EMBED_LINKS', 'SEND_MESSAGES', 'MANAGE_ROLES'],
+			userPermissions: ['MANAGE_ROLES'],
+			channel: 'guild'
+		});
+	}
+	public async exec(message: Message,{ member }: { member: BushGuildMember }): Promise<Message> {
+		if (message.guild.id !== '516977525906341928') {
+			return message.util.reply("<:no:787549684196704257> This command can only be run in Moulberry's Bush.");
+		}
+		const hadRoles = await stickyRoleDataSchema.find({ id: member.id });
+		if (hadRoles && hadRoles.length != 0) {
+			// eslint-disable-next-line @typescript-eslint/no-empty-function
+			const addedRoles = await member.roles.add(hadRoles[0]['roles'], "Returning member's previous roles.").catch(() => {})
+			if (addedRoles){
+				message.util.reply(`<:yes:787549618770149456> Returned <@!${member.user.id}>'s previous roles.`, {allowedMentions: AllowedMentions.none()})
+			}
+		} else {
+			return message.util.reply(`<:no:787549684196704257> <@!${member.user.id}> Does not appear to have any cached roles.`, {allowedMentions: AllowedMentions.none()})
+		}
+	}
+}
+                                                                                                                                                                           
