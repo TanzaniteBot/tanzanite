b25b76dbbe28cb05a3cb8f628ad3f81c518e5c57
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-04-19 17:28:48 -0400

    updated rules

--- src/commands/Moulberry Bush/rule.ts
+++ src/commands/Moulberry Bush/rule.ts
@@ -2,8 +2,6 @@ import { BushCommand } from '../../lib/extensions/BushCommand';
 import AllowedMentions from '../../lib/utils/AllowedMentions';
 import { Message, MessageEmbed, User } from 'discord.js';
 import { Argument } from 'discord-akairo';
-import { TextChannel } from 'discord.js';
-
 export default class RuleCommand extends BushCommand {
 	public constructor() {
 		super('rule', {
@@ -40,100 +38,46 @@ export default class RuleCommand extends BushCommand {
 			channel: 'guild'
 		});
 	}
-	public async exec(message: Message, { rule, user }: { rule: undefined | number; user: User }): Promise<void> {
-		if (message.guild.id !== '516977525906341928') {
-			await message.util.reply("<:no:787549684196704257> This command can only be run in Moulberry's Bush.");
-			return;
-		}
-		// const rulesMessage:MessageEmbed = (await (this.client.channels.cache.get('740441948485189642') as TextChannel).messages.fetch('785162688727875594')).embeds[0]
-		// for (const field in rulesMessage.fields){
+	public exec(message: Message, { rule, user }: { rule: undefined | number; user: User }): unknown {
+		if (message.guild.id !== '516977525906341928') return message.util.reply("<:no:787549684196704257> This command can only be run in Moulberry's Bush.");
 
-		// }
-
-		const rulesEmbed = new MessageEmbed().setColor('ef3929'),
-			rule1a = "1.) Follow Discord's TOS",
-			rule1b =
+		const rulesEmbed = new MessageEmbed().setColor('ef3929');
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const rules = {
+			t1: "1.) Follow Discord's TOS",
+			d1:
 				"Be sure to follow discord's TOS found at <https://discordapp.com/tos>, you must be 13 to use discord so if you admit to being under 13 you will be banned from the server.",
-			rule2a = '2.) Be Respectful',
-			rule2b =
+			t2: '2.) Be Respectful',
+			d2:
 				'Racist, sexist, homophobic, xenophobic, transphobic, ableist, hate speech, slurs, or any other derogatory, toxic, or discriminatory behavior will not be tolerated.',
-			rule3a = '3.) No Spamming',
-			rule3b = 'Including but not limited to: any messages that do not contribute to the conversation, repeated messages, randomly tagging users, and chat flood.',
-			rule4a = '4.) English',
-			rule4b = 'The primary language of the server is English, please keep all discussions in English.',
-			rule5a = '5.) Safe for Work',
-			rule5b = 'Please keep NSFW and NSFL content out of this server, avoid borderline images as well as keeping your status and profile picture SFW.',
-			rule6a = '6.) No Advertising',
-			rule6b = 'Do not promote anything without prior approval from a staff member, this includes DM advertising.',
-			rule7a = '7.) Impersonation',
-			rule7b = 'Do not try to impersonate others for the express intent of being deceitful, defamation , and/or personal gain.',
-			rule8a = '8.) Swearing',
-			rule8b = 'Swearing is allowed only when not used as an insult.',
-			rule9a = '9.) Only ping @Emergency in emergencies',
-			rule9b = 'Pinging <@&791042123608752181> for no reason will result in severe punishment.  <@&791042123608752181> is only to be pinged in true emergencies.',
-			rule10a = '10.) No Backseat Moderating',
-			rule10b = 'If you see a rule being broken be broken, please report it using: `-report <user> [evidence]`.',
-			rule11a = '11.) Staff may moderate at their discretion',
-			rule11b = 'If there are loopholes in our rules, the staff team may moderate based on what they deem appropriate. The staff team holds final discretion.';
+			t3: '3.) No Spamming',
+			d3: 'Including but not limited to: any messages that do not contribute to the conversation, repeated messages, randomly tagging users, and chat flood.',
+			t4: '4.) English',
+			d4: 'The primary language of the server is English, please keep all discussions in English.',
+			t5: '5.) Safe for Work',
+			d5: 'Please keep NSFW and NSFL content out of this server, avoid borderline images as well as keeping your status and profile picture SFW.',
+			t6: '6.) No Advertising',
+			d6: 'Do not promote anything without prior approval from a staff member, this includes DM advertising.',
+			t7: '7.) Impersonation',
+			d7: 'Do not try to impersonate others for the express intent of being deceitful, defamation , and/or personal gain.',
+			t8: '8.) Swearing',
+			d8: 'Swearing is allowed only when not used as an insult.',
+			t9: '9.) Only ping @emergency in emergencies',
+			d9: 'Pinging <@&833802660209229854> for no reason will result in severe punishment.  <@&833802660209229854> is only to be pinged in true emergencies.',
+			t10: '10.) No Backseat Moderating',
+			d10: 'If you see a rule being broken be broken, please report it using: `-report <user> [evidence]`.',
+			t11: '11.) Staff may moderate at their discretion',
+			d11: 'If there are loopholes in our rules, the staff team may moderate based on what they deem appropriate. The staff team holds final discretion.'
+		};
+
 		if (rule) {
-			switch (rule) {
-				case 1:
-					rulesEmbed.addField(rule1a, rule1b);
-					break;
-				case 2:
-					rulesEmbed.addField(rule2a, rule2b);
-					break;
-				case 3:
-					rulesEmbed.addField(rule3a, rule3b);
-					break;
-				case 4:
-					rulesEmbed.addField(rule4a, rule4b);
-					break;
-				case 5:
-					rulesEmbed.addField(rule5a, rule5b);
-					break;
-				case 6:
-					rulesEmbed.addField(rule6a, rule6b);
-					break;
-				case 7:
-					rulesEmbed.addField(rule7a, rule7b);
-					break;
-				case 8:
-					rulesEmbed.addField(rule8a, rule8b);
-					break;
-				case 9:
-					rulesEmbed.addField(rule9a, rule9b);
-					break;
-				case 10:
-					rulesEmbed.addField(rule10a, rule10b);
-					break;
-				case 11:
-					rulesEmbed.addField(rule11a, rule11b);
-					break;
+			eval(`rulesEmbed.addField(rules.t${rule}, rules.d${rule})`);
+		} else {
+			for (let i = 1; i <= 11; i++) {
+				eval(`rulesEmbed.addField(rules.t${i}, rules.d${i})`);
 			}
-		} else {
-			rulesEmbed
-				.addField(rule1a, rule1b)
-				.addField(rule2a, rule2b)
-				.addField(rule3a, rule3b)
-				.addField(rule4a, rule4b)
-				.addField(rule5a, rule5b)
-				.addField(rule6a, rule6b)
-				.addField(rule7a, rule7b)
-				.addField(rule8a, rule8b)
-				.addField(rule9a, rule9b)
-				.addField(rule10a, rule10b)
-				.addField(rule11a, rule11b);
 		}
-		if (!user) {
-			await message.util.reply(rulesEmbed);
-			return;
-		} else {
-			await message.util.reply(`<@!${user.id}>`, {
-				embed: rulesEmbed,
-				allowedMentions: AllowedMentions.users()
-			});
-			return;
-		}
+		if (!user) return message.util.reply(rulesEmbed);
+		else return message.util.reply(`<@!${user.id}>`, { embed: rulesEmbed, allowedMentions: AllowedMentions.users() });
 	}
 }
