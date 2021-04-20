ce4b9f449228a8014e5871291efb170742016f4b
TrashCan69420 <trashcan69666@gmail.com>
2021-04-20 09:34:29 -0400

    formatting so it shuts up

--- src/commands/Moulberry Bush/neu.ts
+++ src/commands/Moulberry Bush/neu.ts
@@ -19,8 +19,6 @@ export default class ReportCommand extends BushCommand {
 		message.channel.send('Please download the latest patch from <#693586404256645231>.');
 		try {
 			message.delete();
-		} catch (e) {
-
-        }
+		} catch (e) {}
 	}
 }
