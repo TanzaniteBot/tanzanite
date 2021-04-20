208e3fd36e21cd18bd7fba109ce2a5d1aaa7d34f
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-04-19 22:01:47 -0400

    yarn format

--- src/commands/Moulberry Bush/giveawayping.ts
+++ src/commands/Moulberry Bush/giveawayping.ts
@@ -30,17 +30,22 @@ export default class GiveawayPingCommand extends BushCommand {
 		//if (message.author.id != '322862723090219008') {
 		if (message.channel.type === 'dm') return message.util.reply('<:no:787549684196704257> This command cannot be run in DMs.');
 		if (message.guild.id !== '516977525906341928') return message.util.reply("<:no:787549684196704257> This command can only be run in Moulberry's Bush.");
-		if (message.channel.id !== ('767782084981817344'|| '833855738501267456')) {return message.util.reply('<:no:787549684196704257> This command can only be run in <#767782084981817344>.');}
+		if (message.channel.id !== ('767782084981817344' || '833855738501267456')) {
+			return message.util.reply('<:no:787549684196704257> This command can only be run in <#767782084981817344>.');
+		}
 		if (!message.member.permissions.has('MANAGE_GUILD')) return message.util.reply('<:no:787549684196704257> You are missing the `MANAGE_GUILD` permission.');
 		//}
 
 		// eslint-disable-next-line @typescript-eslint/no-empty-function
 		await message.delete().catch(() => {});
 		const webhookClient = new WebhookClient(this.client.credentials.webhookID, this.client.credentials.webhookToken);
-		return webhookClient.send('🎉 <@&767782793261875210> Giveaway.\n\n<:mad:783046135392239626> Spamming, line breaking, gibberish etc. disqualifies you from winning. We can and will ban you from giveaways. Winners will all be checked and rerolled if needed.', {
-			username: message.author.tag,
-			avatarURL: message.author.avatarURL({ dynamic: true }),
-			allowedMentions: AllowedMentions.roles()
-		});
+		return webhookClient.send(
+			'🎉 <@&767782793261875210> Giveaway.\n\n<:mad:783046135392239626> Spamming, line breaking, gibberish etc. disqualifies you from winning. We can and will ban you from giveaways. Winners will all be checked and rerolled if needed.',
+			{
+				username: message.author.tag,
+				avatarURL: message.author.avatarURL({ dynamic: true }),
+				allowedMentions: AllowedMentions.roles()
+			}
+		);
 	}
 }
