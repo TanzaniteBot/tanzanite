129d89de9477d263458a37ee824ebd86fa48ee78
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-04-25 12:09:23 -0400

    WHY IS NO WORK!!!

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,8 @@ module.exports = {
 			name: 'BushBot',
 			script: 'yarn',
 			args: 'start',
-			out_file: '/dev/null',
-			error_file: '/dev/null',
-			log_file: 'combined.log',
+			out_file: 'combined.log',
+			error_file: 'combined.log',
 			max_memory_restart: '2000M',
 			node_args: ['--max_old_space_size=2048'],
 			env: {
