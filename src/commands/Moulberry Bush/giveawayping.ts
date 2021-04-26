f0e818713887cd61a621050f9410ac2f8125b2ad
TymanWasTaken <tyman@tyman.tech>
2021-04-26 15:24:07 -0600

    e

--- src/commands/Moulberry Bush/giveawayping.ts
+++ src/commands/Moulberry Bush/giveawayping.ts
@@ -30,9 +30,11 @@ export default class GiveawayPingCommand extends BushCommand {
 		//if (message.author.id != '322862723090219008') {
 		if (message.channel.type === 'dm') return message.util.reply('<:no:787549684196704257> This command cannot be run in DMs.');
 		if (message.guild.id !== '516977525906341928') return message.util.reply("<:no:787549684196704257> This command can only be run in Moulberry's Bush.");
+		/*
 		if (!['767782084981817344', '833855738501267456'].includes(message.channel.id)) {
 			return message.util.reply('<:no:787549684196704257> This command can only be run in a giveaway channel.');
 		}
+		*/
 		if (!message.member.permissions.has('MANAGE_GUILD')) return message.util.reply('<:no:787549684196704257> You are missing the `MANAGE_GUILD` permission.');
 		//}
 
