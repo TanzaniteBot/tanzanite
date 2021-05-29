a57f343e92c4f13b26e4c5b79541547d816e7264
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-05-28 20:15:45 -0400

    commet out, not finished yet

--- src/lib/utils/Logger.ts
+++ src/lib/utils/Logger.ts
@@ -1,43 +1,43 @@
-import { TextChannel } from 'discord.js';
-import { BushClient } from '../extensions/BushClient';
-import chalk from 'chalk';
+// import { TextChannel } from 'discord.js';
+// import { BushClient } from '../extensions/BushClient';
+// import chalk from 'chalk';
 
-export class Logger {
-	private client: BushClient;
-	public constructor(client: BushClient) {
-		this.client = client;
-	}
-	private stripColor(text: string): string {
-		return text.replace(
-			// eslint-disable-next-line no-control-regex
-			/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
-			''
-		);
-	}
-	public getChannel(channel: 'log' | 'error' | 'dm'): Promise<TextChannel> {
-		return this.client.channels.fetch(this.client.config.channels[channel]) as Promise<TextChannel>;
-	}
-	public async log(message: string, sendChannel = false): Promise<void> {
-		console.log(chalk`{bgCyan LOG} ` + message);
-		if (sendChannel) {
-			const channel = await this.getChannel('log');
-			await channel.send('[LOG] ' + this.stripColor(message));
-		}
-	}
+// export class Logger {
+// 	private client: BushClient;
+// 	public constructor(client: BushClient) {
+// 		this.client = client;
+// 	}
+// 	private stripColor(text: string): string {
+// 		return text.replace(
+// 			// eslint-disable-next-line no-control-regex
+// 			/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+// 			''
+// 		);
+// 	}
+// 	public getChannel(channel: 'log' | 'error' | 'dm'): Promise<TextChannel> {
+// 		return this.client.channels.fetch(this.client.config.channels[channel]) as Promise<TextChannel>;
+// 	}
+// 	public async log(message: string, sendChannel = false): Promise<void> {
+// 		console.log(chalk`{bgCyan LOG} ` + message);
+// 		if (sendChannel) {
+// 			const channel = await this.getChannel('log');
+// 			await channel.send('[LOG] ' + this.stripColor(message));
+// 		}
+// 	}
 
-	public async verbose(message: string, sendChannel = false): Promise<void> {
-		if (!this.client.config.verbose) return;
-		console.log(chalk`{bgMagenta VERBOSE} ` + message);
-		if (sendChannel) {
-			const channel = await this.getChannel('log');
-			await channel.send('[VERBOSE] ' + this.stripColor(message));
-		}
-	}
-	public async error(message: string, sendChannel = false): Promise<void> {
-		console.log(chalk`{bgRed ERROR} ` + message);
-		if (sendChannel) {
-			const channel = await this.getChannel('error');
-			await channel.send('[ERROR] ' + this.stripColor(message));
-		}
-	}
-}
+// 	public async verbose(message: string, sendChannel = false): Promise<void> {
+// 		if (!this.client.config.verbose) return;
+// 		console.log(chalk`{bgMagenta VERBOSE} ` + message);
+// 		if (sendChannel) {
+// 			const channel = await this.getChannel('log');
+// 			await channel.send('[VERBOSE] ' + this.stripColor(message));
+// 		}
+// 	}
+// 	public async error(message: string, sendChannel = false): Promise<void> {
+// 		console.log(chalk`{bgRed ERROR} ` + message);
+// 		if (sendChannel) {
+// 			const channel = await this.getChannel('error');
+// 			await channel.send('[ERROR] ' + this.stripColor(message));
+// 		}
+// 	}
+// }
