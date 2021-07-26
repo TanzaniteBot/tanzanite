13ba1ad552047eb9386e91d542a975c4bef58b08
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-07-26 18:44:47 -0400

    fix: ecosystem.config.json -> ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	apps: [
+		{
+			name: 'BushBot-Beta',
+			script: 'yarn',
+			args: 'start',
+			out_file: './combined-dev.log',
+			error_file: './combined-dev.log',
+			max_memory_restart: '2000M',
+			node_args: ['--max_old_space_size=2048'],
+			env: {
+				FORCE_COLOR: '3'
+			}
+		}
+	]
+};
