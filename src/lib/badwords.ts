6bdc8db75f093098ba6c8e0818c006e77ddc6b88
jani270 <69345714+jani270@users.noreply.github.com>
2021-10-27 14:45:11 +0200

    👍  (#29)
    * link
    
    * another one
    
    * another one
    
    * Update badwords.ts
    
    * another one

--- src/lib/badwords.ts
+++ src/lib/badwords.ts
@@ -273,6 +273,13 @@ export default {
 		reason: "discord nitro scam phrase",
 		regex: false,
 	},
+	"Hi i claim this nitro for free 3 months lol!": {
+		severity: Severity.PERM_MUTE,
+		ignoreSpaces: true,
+		ignoreCapitalization: true,
+		reason: "discord nitro scam phrase",
+		regex: false,
+	},
 
 	/* -------------------------------------------------------------------------- */
 	/*                                 Misc Scams                                 */
