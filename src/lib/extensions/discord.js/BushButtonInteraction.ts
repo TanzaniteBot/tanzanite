43dadce8b744a43b86cc3febba1046d714cdafcb
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-10-27 17:23:36 -0400

    fix breaking changes

--- src/lib/extensions/discord.js/BushButtonInteraction.ts
+++ src/lib/extensions/discord.js/BushButtonInteraction.ts
@@ -1,15 +1,15 @@
 import { APIInteractionGuildMember } from 'discord-api-types/v9';
-import { ButtonInteraction } from 'discord.js';
+import { ButtonInteraction, CacheType, CacheTypeReducer } from 'discord.js';
 import { RawMessageButtonInteractionData } from 'discord.js/typings/rawDataTypes';
 import { BushClient, BushTextBasedChannels } from '../discord-akairo/BushClient';
 import { BushGuild } from './BushGuild';
 import { BushGuildMember } from './BushGuildMember';
 import { BushUser } from './BushUser';
 
-export class BushButtonInteraction extends ButtonInteraction {
+export class BushButtonInteraction<Cached extends CacheType = CacheType> extends ButtonInteraction<Cached> {
 	public declare readonly channel: BushTextBasedChannels | null;
-	public declare readonly guild: BushGuild | null;
-	public declare member: BushGuildMember | APIInteractionGuildMember | null;
+	public declare readonly guild: CacheTypeReducer<Cached, BushGuild, null>;
+	public declare member: CacheTypeReducer<Cached, BushGuildMember, APIInteractionGuildMember>;
 	public declare user: BushUser;
 	public constructor(client: BushClient, data: RawMessageButtonInteractionData) {
 		super(client, data);
--- src/lib/extensions/discord.js/BushMessage.ts
+++ src/lib/extensions/discord.js/BushMessage.ts
@@ -1,6 +1,6 @@
-import { Message, Partialize } from 'discord.js';
+import { If, Message, Partialize } from 'discord.js';
 import { RawMessageData } from 'discord.js/typings/rawDataTypes';
-import { BushClient, BushTextBasedChannels } from '../discord-akairo/BushClient';
+import { BushClient, BushGuildTextBasedChannel, BushTextBasedChannels } from '../discord-akairo/BushClient';
 import { BushCommandUtil } from '../discord-akairo/BushCommandUtil';
 import { BushGuild } from './BushGuild';
 import { BushGuildMember } from './BushGuildMember';
@@ -11,13 +11,13 @@ export type PartialBushMessage = Partialize<
 	'type' | 'system' | 'pinned' | 'tts',
 	'content' | 'cleanContent' | 'author'
 >;
-export class BushMessage extends Message {
+export class BushMessage<Cached extends boolean = boolean> extends Message<Cached> {
 	public declare readonly client: BushClient;
 	public override util!: BushCommandUtil<BushMessage>;
 	public declare readonly guild: BushGuild | null;
 	public declare readonly member: BushGuildMember | null;
 	public declare author: BushUser;
-	public declare readonly channel: BushTextBasedChannels;
+	public declare readonly channel: If<Cached, BushGuildTextBasedChannel, BushTextBasedChannels>;
 	public constructor(client: BushClient, data: RawMessageData) {
 		super(client, data);
 		// this.util = new BushCommandUtil(client.commandHandler, this);
