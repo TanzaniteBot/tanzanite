142fe78f76d1e04b480da09f5e36cb70b98e24ff
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2022-03-13 21:55:10 -0400

    fix(supportThread): don't trigger on zepplin messages

---
+++ src/listeners/bush/supportThread.ts
@@ -0,0 +1,63 @@
+import { BushListener, BushTextChannel, type BushClientEvents } from '#lib';
+import assert from 'assert';
+import { stripIndent } from 'common-tags';
+import { Embed, MessageType, PermissionFlagsBits } from 'discord.js';
+
+export default class SupportThreadListener extends BushListener {
+	public constructor() {
+		super('supportThread', {
+			emitter: 'client',
+			event: 'messageCreate',
+			category: 'bush'
+		});
+	}
+
+	public override async exec(...[message]: BushClientEvents['messageCreate']): Promise<Promise<void> | undefined> {
+		if (!client.config.isProduction || !message.inGuild()) return;
+		if (![MessageType.Default, MessageType.Reply].includes(message.type)) return;
+		if (message.thread) return;
+		if (message.author.bot && (message.author.id !== '444871677176709141' || !message.content.includes('uploaded a log,')))
+			return;
+
+		if (message.guild.id !== '516977525906341928') return; // mb
+		if (message.channel.id !== '714332750156660756') return; // neu-support
+
+		if (
+			[await message.guild.getSetting('prefix'), `<@!${client.user!.id}>`, `<@${client.user!.id}>`].some((v) =>
+				message.content.trim().startsWith(v)
+			) &&
+			client.commandHandler.aliases.some((alias) => message.content.includes(alias))
+		)
+			return;
+
+		assert(message.channel instanceof BushTextChannel);
+
+		if (!message.channel.permissionsFor(message.guild.me!).has(PermissionFlagsBits.CreatePublicThreads)) return;
+		const thread = await message
+			.startThread({
+				name: `Support - ${message.author.username}＃${message.author.discriminator}`,
+				autoArchiveDuration: 60,
+				reason: 'Support Thread'
+			})
+			.catch(() => null);
+		if (!thread) return;
+		const embed = new Embed()
+			.setTitle('NotEnoughUpdates Support')
+			.setDescription(
+				stripIndent`
+				Welcome to Moulberry Bush Support:tm:
+				
+				Please make sure you have the latest version found in <#693586404256645231>.
+				Additionally if you need help installing the mod be sure to read <#737444942724726915> for a guide on how to do so.`
+			)
+			.setColor(util.colors.Blurple);
+		void thread
+			.send({ embeds: [embed] })
+			.then(() =>
+				client.console.info(
+					'supportThread',
+					`opened a support thread for <<${message.author.tag}>> in <<${message.channel.name}>> in <<${message.guild!.name}>>.`
+				)
+			);
+	}
+}
