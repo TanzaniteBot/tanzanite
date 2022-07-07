3b229fecbd71ac98bf17bb7a41bcb53cbbc5510c
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2022-07-07 20:22:54 +0200

    extract needed part of node-os-utils and uninstall it

--- src/tasks/cpuUsage.ts
+++ src/tasks/cpuUsage.ts
@@ -1,5 +1,5 @@
 import { BushTask, Time } from '#lib';
-import osu from 'node-os-utils';
+import os from 'node:os';
 
 export default class CpuUsageTask extends BushTask {
 	public constructor() {
@@ -10,7 +10,53 @@ export default class CpuUsageTask extends BushTask {
 	}
 
 	public async exec() {
-		const cpuStats = await osu.cpu.usage(this.client.stats.cpu === undefined ? 100 * Time.Millisecond : Time.Minute);
+		const cpuStats = await cpu.usage(this.client.stats.cpu === undefined ? 100 * Time.Millisecond : Time.Minute);
 		this.client.stats.cpu = cpuStats;
+	}
+}
+
+/* Everything inside the cpu namespace is adapted from the "node-os-utils" npm package which is licensed under a MIT license by Sunil Wang */
+namespace cpu {
+	export function usage(interval = Time.Second): Promise<number> {
+		return new Promise((resolve) => {
+			const startMeasure = average();
+
+			setTimeout(() => {
+				const endMeasure = average();
+				const idleDifference = endMeasure.avgIdle - startMeasure.avgIdle;
+				const totalDifference = endMeasure.avgTotal - startMeasure.avgTotal;
+				const cpuPercentage = (10000 - Math.round((10000 * idleDifference) / totalDifference)) / 100;
+
+				return resolve(cpuPercentage);
+			}, interval);
+		});
+	}
+
+	export function average(): CpuAverageInfo {
+		let totalIdle = 0;
+		let totalTick = 0;
+		const cpus = os.cpus();
+
+		for (let i = 0, len = cpus.length; i < len; i++) {
+			const cpu = cpus[i];
+			for (const type in cpu.times) {
+				totalTick += cpu.times[type as keyof typeof cpu.times];
+			}
+			totalIdle += cpu.times.idle;
+		}
+
+		return {
+			totalIdle: totalIdle,
+			totalTick: totalTick,
+			avgIdle: totalIdle / cpus.length,
+			avgTotal: totalTick / cpus.length
+		};
+	}
+
+	export interface CpuAverageInfo {
+		totalIdle: number;
+		totalTick: number;
+		avgIdle: number;
+		avgTotal: number;
 	}
 }
