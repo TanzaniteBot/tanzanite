6d4e1f072ae9e0f837c91193a9a8bddf038e93f6
IRONM00N <me@ironmoon.dev>
2025-05-20 01:50:49 -0400

    wip reminders

--- src/commands/utilities/reminders.ts
+++ src/commands/utilities/reminders.ts
@@ -4,6 +4,7 @@ import {
 	BotCommand,
 	ButtonPaginator,
 	Reminder,
+	Time,
 	chunk,
 	colors,
 	emojis,
@@ -11,7 +12,8 @@ import {
 	type CommandMessage,
 	type SlashMessage
 } from '#lib';
-import type { APIEmbed } from 'discord.js';
+import { StringSelectMenuBuilder } from '@discordjs/builders';
+import { ActionRowBuilder, type APIEmbed } from 'discord.js';
 import assert from 'node:assert/strict';
 import { Op } from 'sequelize';
 
@@ -38,7 +40,9 @@ export default class RemindersCommand extends BotCommand {
 		const reminders = await Reminder.findAll({ where: { user: message.author.id, expires: { [Op.gt]: new Date() } } });
 		if (!reminders.length) return message.util.send(`${emojis.error} You don't have any reminders set.`);
 
-		const formattedReminders = reminders.map((reminder) => `${timestamp(reminder.expires, 't')} - ${reminder.content}`);
+		const formattedReminders = reminders.map(
+			(reminder, i) => `[\`${i}\`] ${timestamp(reminder.expires, 't')} - ${reminder.content}`
+		);
 
 		const chunked = chunk(formattedReminders, 15);
 		const embeds: APIEmbed[] = chunked.map((chunk) => ({
@@ -46,6 +50,17 @@ export default class RemindersCommand extends BotCommand {
 			description: chunk.join('\n'),
 			color: colors.default
 		}));
-		return await ButtonPaginator.send(message, embeds);
+		return await ButtonPaginator.send(message, embeds, {}, true, 1, 5 * Time.Minute, [
+			new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+				new StringSelectMenuBuilder({
+					custom_id: 'manage-reminder',
+					placeholder: 'Select a reminder to manage',
+					options: reminders.map((reminder, i) => ({
+						label: `[${i}] ${timestamp(reminder.expires, 't')}`,
+						value: i.toString()
+					}))
+				})
+			)
+		]);
 	}
 }
